287eaff79dc3ccdf6b3267ea779de297f8e2d05a
Simon Newton <simonanewton@gmail.com>
2020-07-28 16:09:32 -0400

    added portfolio, resume, and contact pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,19 +3,19 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.sendfile(path.join(__dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 router.get("/portfolio", (req, res) => {
-
+    res.sendFile(path.join(__dirname, "../public/portfolio.html"));
 });
 
 router.get("/resume", (req, res) => {
-    
+    res.sendFile(path.join(__dirname, "../public/resume.html"));
 });
 
 router.get("/contact", (req, res) => {
-    
+    res.sendFile(path.join(__dirname, "../public/contact.html"));
 });
 
 module.exports = router;
